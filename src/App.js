08c3867329f014e45de0e550f00f3c37de1ce0e7
Davia <davia@DeNisco.local>
2018-05-31 21:47:56 -0400

    Conditionally display user info

--- src/App.js
+++ src/App.js
@@ -11,58 +11,43 @@ class App extends Component {
     this.state = {
       json: json,
       userSelected: false,
-      user: ""
+      user: false
     }
     this.handleClick = this.handleClick.bind(this)
   };
 
-  returnUserInfo(id) {
-    const userSelected = this.state.userSelected;
-    console.log("1. userSelected", userSelected);
-
+  retreiveUserInfo(id) {
     for (let i = 0; i < json.length; i++) {
       if (json[i].id === parseInt(id)) {
-        this.state.user = json[i];
-        const currentUser = this.state.user;
-        console.log("5. id", id);
         this.setState({
-          userSelected: userSelected
+          user: json[i]
         })
-        console.log("2. json obj: ", this.state.user);
-        console.log("3. userSelected: ", this.state.userSelected)
-
-        console.log("4. first name: ", this.state.user.first)
-
       }
-
     }
   }
 
   handleClick(e) {
-    this.id = e.target.id;
+    this.retreiveUserInfo(e.target.id);
+  }
 
-    this.returnUserInfo(e.target.id);
-
-    this.setState({
-       userSelected: true
-    })
+  showInfo(user) {
+    return this.state.user && this.state.user.id == user.id
   }
 
   render() {
-
     return this.state.json.map((user, index) =>
-    <div>
-      <div key={user.index}>
-        <Button className="user" id={user.id} onClick={this.handleClick}>{user.first} {user.last}</Button>
-      </div>
-      {this.state.userSelected ? (
-        <div className="display">
-          <p>{this.state.user.first} {this.state.user.last}</p>
-          <p>{this.state.user.age}</p>
-          <p>{this.state.user.location}</p>
-          <p>{this.state.user.description}</p>
+      <div>
+        <div key={user.index}>
+          <Button className="user" id={user.id} onClick={this.handleClick}>{user.first} {user.last}</Button>
         </div>
-      ) : null}
+        { this.showInfo(user) ? (
+          <div className="display">
+            <p>{this.state.user.first} {this.state.user.last}</p>
+            <p>{this.state.user.age}</p>
+            <p>{this.state.user.location}</p>
+            <p>{this.state.user.description}</p>
+          </div>
+        ) : null}
       </div>
     );
   }
