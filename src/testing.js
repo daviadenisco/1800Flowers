bc9a5073c6a748901e12569f0ca4b9922f903bb3
Davia <davia@DeNisco.local>
2018-06-02 15:52:16 -0400

    work on testing file

--- src/testing.js
+++ src/testing.js
@@ -1,30 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { configure } from 'enzyme';
+import Enzyme from 'enzyme';
+import { shallow, mount, configure, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import toJson from "enzyme-to-json";
 import App from './App';
-import { shallow, mount, render } from 'enzyme';
 var json = require('./json.js').json;
 
 configure({ adapter: new Adapter() });
 
-const wrapper = shallow(<App />);
-
-const userDataStore = mount(<App first={John} additionalInfo={retreiveUserInfo} />
-);
 const instance = userDataStore.getElement().props;
 
 describe("<App />", () => {
-  it("renders correctly", () => {
-    expect(toJson(userDataStore)).toMatchSnapshot();
-  });
+  let userDataStore = mount(<App user={{ first: 'test user' }} />);
 
-  it("has `retreiveUserInfo` as props", () => {
-    expect(userDataStore.props().selectedUser).toEqual(retreiveUserInfo)
+  it('renders the user first name', () => {
+    // expect(toJson(userDataStore)).toMatchSnapshot();
+    console.log(user.debug());
   });
-
-  it("has `first` as props", () => {
-    expect(retreiveUserInfo.props().first).toEqual(John)
-  });
+  //
+  // it("has `retreiveUserInfo` as props", () => {
+  //   expect(userDataStore.props().selectedUser).toEqual(retreiveUserInfo)
+  // });
+  //
+  // it("has `first` as props", () => {
+  //   expect(retreiveUserInfo.props().first).toEqual(John)
+  // });
 });
