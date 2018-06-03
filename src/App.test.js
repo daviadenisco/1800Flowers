a8605a834c4995e5dfee22b5cea81b0339f9885d
Davia <davia@DeNisco.local>
2018-06-03 18:39:07 -0400

    install previous version of jest-cli to prevent conflict with React

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme from 'enzyme';
+import { shallow, mount, configure, render } from 'enzyme';
 import App from './App';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe('App', () => {
+  let app = mount(<App app={{ user: 'test user first' }}/>);
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+    console.log(app.debug());
+  });
+
+  it('renders the first name', () => {
+    expect(app.find('p').text()).toEqual('John Roberts');
+  });
 });
