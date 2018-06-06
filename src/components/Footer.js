d93d9d241664f453af01201414d5a1c7071a37a8
Davia <davia@DeNisco.local>
2018-06-05 20:47:10 -0400

    update tests

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
 
     <div>
-      <a href="mailto:davia622@gmail.com?Subject=You%20are%20hired" target="_blank=">
+      <a href="mailto:davia622@gmail.com?Subject=You%20are%20hired">
         <h1 className="footer">Contact Us</h1>
       </a>
     </div>
